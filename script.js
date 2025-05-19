bc7dadec4e45820a0d5c8b72d089b0dfa29b9722
SidersMini <tanguy.duthion@gmail.com>
2025-05-19 23:03:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+/* Aucune logique JS nécessaire pour l’animation.
+   Le fichier est laissé ici « vide » pour respecter la structure
+   trois-fichiers demandée et faciliter d’éventuels ajouts. */
+console.log("Animation soleil : fichier script.js chargé (aucune action JS nécessaire).");
